cd8958c230873778e75b0242b38997663be68e94
赵晏龙 <kitten63928@qq.com>
2019-07-18 10:17:29 +0800

    更新获取IP的方式

--- get_ip.py
+++ get_ip.py
@@ -1,6 +1,7 @@
 import re
 import logging
 import socket
+import json
 from urllib import request, error, parse
 
 # 匹配合法 IP 地址
@@ -14,17 +15,31 @@ regex_ip = re.compile(
 
 # 增强鲁棒性，用多种方式获取 IP
 def get_ip():
-    return (get_ip_by_ipip()
+    return (get_ip_by_taobao()
+        or  get_ip_by_ipip()
         or  get_ip_by_httpbin()
-        or  get_ip_by_httpbin_direct_1()
-        or  get_ip_by_httpbin_direct_2() )
+        or  get_ip_by_httpbin_direct_1() )
     
 # 这几个函数会在 DNS 遭受污染时失效
+def get_ip_by_taobao():
+    url = 'http://ip.taobao.com/service/getIpInfo.php?ip=myip'
+    try:
+        resp = request.urlopen(url=url, timeout=10).read()
+        jsonBody = json.loads(resp.decode("utf-8"))
+        ip = jsonBody['data']['ip']
+        logging.info("get ip by taobao: %s" % ip)
+        return ip
+    except Exception as e:
+        logging.warning("get_ip_by_taobao FAILED, error: %s", str(e))
+        return None
+
 def get_ip_by_ipip():
     url = 'http://myip.ipip.net/'
     try:
         resp = request.urlopen(url=url, timeout=10).read()
-        return regex_ip.match(resp.decode("utf-8")).group(1)
+        ip = regex_ip.match(resp.decode("utf-8")).group(1)
+        logging.info("get ip by ipip: %s" % ip)
+        return ip
     except Exception as e:
         logging.warning("get_ip_by_ipip FAILED, error: %s", str(e))
         return None
@@ -33,7 +48,9 @@ def get_ip_by_httpbin():
     url = 'http://www.httpbin.org/ip'
     try:
         resp = request.urlopen(url=url, timeout=10).read()
-        return regex_ip.match(resp.decode("utf-8")).group(1)
+        ip = regex_ip.match(resp.decode("utf-8")).group(1)
+        logging.info("get ip by httpbin: %s" % ip)
+        return ip
     except Exception as e:
         logging.warning("get_ip_by_httpbin FAILED, error: %s", str(e))
         return None
@@ -41,29 +58,22 @@ def get_ip_by_httpbin():
 # 这个函数可以在本地 DNS 遭受污染的时候获取到IP
 # 如需模拟DNS污染，可以在HOSTS文件里加入 127.0.0.1 www.httpbin.org
 def get_ip_by_httpbin_direct_1():
-    url = 'http://52.5.182.176/ip'
+    url = 'http://52.201.109.155/ip'
     try:
         req = request.Request(url=url, method='GET', headers={'Host': 'www.httpbin.org'})
         resp = request.urlopen(req).read()
-        return regex_ip.match(resp.decode("utf-8")).group(1)
+        ip = regex_ip.match(resp.decode("utf-8")).group(1)
+        logging.info("get ip by httpbin_direct_1: %s" % ip)
+        return ip
     except Exception as e:
         logging.warning("get_ip_by_httpbin_direct_1 FAILED, error: %s", str(e))
-        return None
-
-def get_ip_by_httpbin_direct_2():
-    url = 'http://52.44.230.61/ip'
-    try:
-        req = request.Request(url=url, method='GET', headers={'Host': 'www.httpbin.org'})
-        resp = request.urlopen(req).read()
-        return regex_ip.match(resp.decode("utf-8")).group(1)
-    except Exception as e:
-        logging.warning("get_ip_by_httpbin_direct_2 FAILED, error: %s", str(e))
         return None
 
     
 # 测试
 if __name__ == '__main__':
     print(get_ip()                     )
+    print(get_ip_by_taobao()             )
     print(get_ip_by_ipip()             )
     print(get_ip_by_httpbin()          )
     print(get_ip_by_httpbin_direct_1() )
